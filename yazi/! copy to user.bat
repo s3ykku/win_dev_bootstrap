91cd73ed1c9ab6d65c3c947e91d78877bebd751b
s3ykku-work <s3ykku@gmail.com>
2026-06-30 17:03:57 +0300

    update yazi config, add fullborder and git plugins, and kanagawa theme

--- yazi/! copy to user.bat
+++ yazi/! copy to user.bat
@@ -1,2 +1,4 @@
 @echo off
 xcopy "%~dp0yazi.toml" "%USERPROFILE%AppData\Roaming\yazi\config\" /Y
+xcopy "%~dp0theme.toml" "%USERPROFILE%AppData\Roaming\yazi\config\" /Y
+xcopy "%~dp0init.lua" "%USERPROFILE%AppData\Roaming\yazi\config\" /Y
--- yazi/! update from user.bat
+++ yazi/! update from user.bat
@@ -1,2 +1,4 @@
 @echo off
 xcopy "%USERPROFILE%AppData\Roaming\yazi\config\yazi.toml" "%~dp0" /Y
+xcopy "%USERPROFILE%AppData\Roaming\yazi\config\theme.toml" "%~dp0" /Y
+xcopy "%USERPROFILE%AppData\Roaming\yazi\config\init.lua" "%~dp0" /Y
