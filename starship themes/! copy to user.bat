be3b7dc94aeddd9c49f7c135098aa47090a80662
s3ykku-work <s3ykku@gmail.com>
2026-08-09 16:47:32 +0300

    finally, tokyo night)

--- starship themes/! copy to user.bat
+++ starship themes/! copy to user.bat
@@ -1,4 +1,5 @@
 @echo off
 xcopy "%~dp0starship_kanagawa.toml" "%USERPROFILE%\.config\" /Y
-xcopy "%~dp0starship_tokyonight.toml" "%USERPROFILE%\.config\" /Y
+xcopy "%~dp0starship_tokyonight_blue.toml" "%USERPROFILE%\.config\" /Y
+xcopy "%~dp0starship_tokyonight_storm.toml" "%USERPROFILE%\.config\" /Y
 xcopy "%~dp0starship_gruvbox_dark.toml" "%USERPROFILE%\.config\" /Y
--- starship themes/! update from user.bat
+++ starship themes/! update from user.bat
@@ -1,4 +1,5 @@
 @echo off
 xcopy "%USERPROFILE%\.config\starship_kanagawa.toml" "%~dp0" /Y
-xcopy "%USERPROFILE%\.config\starship_tokyonight.toml" "%~dp0" /Y
+xcopy "%USERPROFILE%\.config\starship_tokyonight_blue.toml" "%~dp0" /Y
+xcopy "%USERPROFILE%\.config\starship_tokyonight_storm.toml" "%~dp0" /Y
 xcopy "%USERPROFILE%\.config\starship_gruvbox_dark.toml" "%~dp0" /Y
