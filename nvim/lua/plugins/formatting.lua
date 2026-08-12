1ef8efc072d6754256cfefa8dbe1a303183defd7
s3ykku-home <s3ykku@gmail.com>
2026-08-12 19:27:53 +0300

    format_on_save in neovim + line endings in zed

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -14,6 +14,10 @@ return {
       },
     },
     opts = {
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
       formatters_by_ft = {
         lua = { "stylua" },
         rust = { "rustfmt" },
